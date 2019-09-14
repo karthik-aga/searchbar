d2189d144d40b291cdad71a9c040a8fcffb4fc75
Karthik Agarwal <karthil1998@gmail.com>
2019-09-14 20:09:02 +0530

    first up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import IntegrationDownshift from './components/search'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#0b46db',
+      main: '#171616',
+      dark: '#0b46db',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#0b46db',
+      main: '#f44336',
+      dark: '#0d0d0d',
+      contrastText: '#000',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme = { theme }>
+      {/* <Navbar /> */}
+      <IntegrationDownshift />
+      {/* <Example />
+      <DownshiftTwo />
+      <Test></Test> */}
+   </MuiThemeProvider>
   );
 }
 
